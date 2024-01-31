d61b3f1f9d356f9fe975fa9945688e3a64715341
mishaRomanov <mishanyaromanov910@gmail.com>
2024-01-31 21:44:50 +0300

    add new handler

--- cmd/main.go
+++ cmd/main.go
@@ -3,22 +3,25 @@ package main
 import (
 	"context"
 	"fmt"
+	"github.com/mishaRomanov/redis-project/internal/handlers"
+	//
 	"github.com/labstack/echo/v4"
 	"github.com/mishaRomanov/redis-project/config"
 	"github.com/redis/go-redis/v9"
-	"log"
-	"net/http"
+	"github.com/sirupsen/logrus"
 )
 
 func main() {
 	//creating a service
 	service := echo.New()
+
 	//creating context
 	ctx := context.Background()
 
+	//parsing env variables into config
 	cfg, err := config.Init()
 	if err != nil {
-		log.Fatal(err)
+		logrus.Fatalf("%v", err)
 	}
 
 	//creating a redis instance
@@ -27,17 +30,24 @@ func main() {
 		Password: cfg.Password,
 		DB:       cfg.DB,
 	})
+	// testing whether redis connection was successful or not
 	pong, err := dbredis.Ping(ctx).Result()
 	if err != nil {
-		log.Fatalf("%v", err)
+		logrus.Fatalf("%v", err)
 	}
-	log.Println(pong)
+	logrus.Println(pong)
 
-	service.GET("/about", func(ctx echo.Context) error {
-		return ctx.String(http.StatusOK, "Hello world!")
-	})
-	fatal_error := service.Start(":8080")
-	if fatal_error != nil {
-		log.Fatalf("%v", fatal_error)
+	//creating handler instance by inserting database object inside
+	handlerService := handlers.NewHandler(dbredis)
+	//handle get /about
+	service.GET("/about", handlerService.Info)
+
+	//handle POST /add
+	service.POST("/add", handlerService.InsertValue)
+
+	//starting a service and catching error
+	serviceError := service.Start(":8080")
+	if serviceError != nil {
+		logrus.Fatalf("%v", serviceError)
 	}
 }

96d489a6bccece8040c89248c334ccd5be5bdbc2
mishaRomanov <mishanyaromanov910@gmail.com>
2024-02-05 21:20:09 +0300

    update config file

--- config/config.go
+++ config/config.go
@@ -2,23 +2,23 @@ package config
 
 import (
 	"github.com/caarlos0/env/v10"
-	"log"
+	"github.com/sirupsen/logrus"
 )
 
 // config struct
 type Config struct {
 	Port     string `env:"PORT"`
 	Password string `env:"PASSWORD"`
-	DB       int    `env:"DB"`
+	DB       int    `env:"DB" envDefault:"0"`
 }
 
-//func that inits config and returns it
+// func that inits config and returns it
 func Init() (Config, error) {
 	var cfg Config
 	//parsing env variables
 	err := env.Parse(&cfg)
 	if err != nil {
-		log.Fatalf("error while parsing environmental variables: %w/n", err)
+		logrus.Fatalf("error while parsing environmental variables: %w/n", err)
 		return Config{}, err
 	}
 	return cfg, nil
